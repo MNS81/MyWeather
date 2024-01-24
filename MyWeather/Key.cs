fa1dc9de264d6585c348df601d00a4bd7424cdf4
MNS81 <0x4c0656dc@gmail.com>
2024-01-24 16:47:43 +0300

    Добавьте файлы проекта.

---
+++ MyWeather/Key.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyWeather
+{
+    internal class Key
+    {
+        public const string API = "1a924564265d89132618b79e397d2cdb";
+    }
+}
---
+++ MyWeather/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using Xamarin.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace MyWeather
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage() => InitializeComponent();
+        private async void cityBtnPress_Clicked(object sender, EventArgs e)
+        {
+            string city = cityInput.Text.Trim();
+            if (city.Length < 2 || string.IsNullOrEmpty(city))
+            {
+                await DisplayAlert("Ошибка", "Название города менее 2-х символов!", "хорошо");
+                return;
+            }
+            HttpClient client = new HttpClient();
+            string request = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={Key.API}&units=metric";
+            string responce = await client.GetStringAsync(request);
+            var json = JObject.Parse(responce);
+            var temp = json["main"]["temp"].ToString();
+            var real = json["main"]["feels_like"].ToString();
+            resultText.Text = $"Температура: {temp}\nПо ощущениям: {real}";
+        }
+    }
+}
